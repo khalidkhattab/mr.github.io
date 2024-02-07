896e01c5aa281852d305086bd5667b9fe5aeb131
khalidkhattab <102302183+khalidkhattab@users.noreply.github.com>
2024-02-08 00:45:24 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,8 +33,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "ee4e3e80ed34f0adde45b095ab85c072
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0080433cf1485fbeb4eafcbd3f1bf287",
-"/": "0080433cf1485fbeb4eafcbd3f1bf287",
+"index.html": "da6a1b2ae881ae55fb7a8ffea622f7bf",
+"/": "da6a1b2ae881ae55fb7a8ffea622f7bf",
 "main.dart.js": "e09688dec2043efc3186f1cc092e2285",
 "manifest.json": "0dcfebee893de8ecf194ce9511510c60",
 "version.json": "1babe854379db2fff661f2b30d1f1c0c"};
